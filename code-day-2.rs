39617baa8cb1818dc280deb4a968148ff27965c6
jennaose <jennifer.oseghale@eng.uniben.edu>
2024-10-17 09:21:02 +0100

    Create code-day-2.rs

---
+++ code-day-2.rs
@@ -0,0 +1,12 @@
+          fn main() {
+           //immutable variables
+           let age= 10;
+           //this is automatically an immutable  variable 
+           
+          println!("age {} ", age);
+          // mutable variables
+           let mut age2= 16;
+           println!("before= {} ", age2);
+           age2= 20;
+           println!("after= {} ", age2)
+          }
