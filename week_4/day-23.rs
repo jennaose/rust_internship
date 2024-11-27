fb1225f4ae2005b0b041b3991610d85f7d2bfafc
jennaose <jennifer.oseghale@eng.uniben.edu>
2024-11-27 13:43:33 +0100

    Update day-23.rs

--- week_4/day-23.rs
+++ week_4/day-23.rs
@@ -1,12 +1,12 @@
 use tokio::time::{sleep,Duration};
 #[tokio::main]
 async fn main(){
-    let task1 = tokio::spawn(async{
-        sleep(Duration::from_secs(3)).await;
+    let task1 = tokio::spawn(async{ //spawan starts a new task
+        sleep(Duration::from_secs(3)).await; //sleep stimulates asynchronous delay(work)
         println!("Task1 completed");
     });
     let task2 = tokio::spawn(async{
-        sleep(Duration::from_secs(2)).await;
+        sleep(Duration::from_secs(2)).await; //.await paused the task until the condition is done 
         println!("Task2 completed");
     });
 
