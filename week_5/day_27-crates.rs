d7861b65db7939af1fc87831d474b9d975da0796
jennaose <jennifer.oseghale@eng.uniben.edu>
2024-12-08 09:25:17 +0100

    Update day_27-crates.rs

--- week_5/day_27-crates.rs
+++ week_5/day_27-crates.rs
@@ -3,8 +3,12 @@ fn main((){
 let json_data =r#"{
 "name":"Rust",
 "type"::"Programming Language"}"#
-
 let parsed : Value = serde_json::from_str(json_data).expect("Failed to parse json");
 println!("Name:{}", parsed ["name"]);
 println!("Type:{}", parsed ["type"]};
 }
+/*
+The output will be
+Name:Rust
+Type: Programming Language
+*/
