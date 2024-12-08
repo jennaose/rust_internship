562f1427163d0e1ab57fff00ad28e1a9b57bdee7
jennaose <jennifer.oseghale@eng.uniben.edu>
2024-12-08 08:42:09 +0100

    Update day_28_crates.rs

--- week_5/day_28_crates.rs
+++ week_5/day_28_crates.rs
@@ -1,10 +1,1 @@
-use serde_json::Value;
-fn main((){
-let json_data =r#"{
-"name":"Rust",
-"type"::"Programming Language"}"#
 
-let parsed : Value = serde_json::from_str(json_data).expect("Failed to parse json");
-println!("Name:{}", parsed ["name"]);
-println!("Type:{}", parsed ["type"]};
-}
